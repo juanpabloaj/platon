bbe720a1ae640f4fd0792d9002905abd88462268
JuanPablo <jpabloaj@gmail.com>
2014-12-27 00:24:38 -0300

    all angular in app.js

--- public/app.js
+++ public/app.js
@@ -1,10 +1,27 @@
+
 app = angular.module('platonApp', ['firebase']);
 
-app.factory('Slide', ['$firebase', function($firebase) {
-  return function(slide){
-    var ref = new Firebase("https://platon.firebaseio.com/slides/")
-      .child(slide);
+app
+  .factory('Slide', ['$firebase', function($firebase) {
+    return function(slide){
+      var ref = new Firebase("https://platon.firebaseio.com/slides/")
+        .child(slide);
 
-    return $firebase(ref).$asObject();
-  };
-}]);
+      return $firebase(ref).$asObject();
+    };
+  }])
+  .controller('SlideEditController', ['$scope', 'Slide',
+    function($scope, Slide){
+      var hashId = document.getElementById('hashId').getAttribute('value');
+      Slide(hashId).$bindTo($scope, 'slide');
+    }
+  ])
+  .controller('SlideShowController', ['$scope',
+    function($scope){
+      var hashId = document.getElementById('hashId').getAttribute('value');
+      var ref = new Firebase("https://platon.firebaseio.com/slides/");
+      ref.child(hashId).on('value', function(snap){
+        slideshow.loadFromString(snap.val().markdown);
+      });
+    }
+  ]);
