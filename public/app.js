e0e245d11627f96047a6e47faaa367f93d6b593b
JuanPablo <jpabloaj@gmail.com>
2015-01-01 17:34:49 -0300

    angularfire to 0.9.0 version fix deprecated msg
    * angularfire to 0.9.0 to fix deprecated message.
    * watch in $scope.slide to ng-change with markdownChanged.
    * Slide factory name to slideFactory.
    * app.js more jshint.

--- public/app.js
+++ public/app.js
@@ -2,19 +2,20 @@
 app = angular.module('platonApp', ['firebase']);
 
 app
-  .factory('Slide', ['$firebase', function($firebase) {
+  .factory('slideFactory', ['$firebase', function($firebase) {
     return function(slide){
-      var ref = new Firebase("https://platon.firebaseio.com/slides/")
+      var ref = new Firebase('https://platon.firebaseio.com/slides/')
         .child(slide);
 
       return $firebase(ref).$asObject();
     };
   }])
-  .controller('SlideEditController', ['$scope','$sce', 'Slide',
-    function($scope, $sce, Slide){
+  .controller('SlideEditController', ['$scope','$sce', 'slideFactory',
+    function($scope, $sce, slideFactory){
       var hashId = document.getElementById('hashId').getAttribute('value');
-      Slide(hashId).$bindTo($scope, 'slide');
-      $scope.$watch('slide', function() {
+      slideFactory(hashId).$bindTo($scope, 'slide');
+
+      $scope.markdownChanged = function() {
         if ($scope.slide){
           $scope.previews = [];
           var markdownSlides = $scope.slide.markdown.split('---\n');
@@ -22,13 +23,15 @@ app
             this.push($sce.trustAsHtml(remark.convert(markdownSlide)));
           }, $scope.previews);
         }
-      });
+      };
+
+      $scope.$watch('slide', $scope.markdownChanged);
     }
   ])
   .controller('SlideShowController', ['$scope',
     function($scope){
       var hashId = document.getElementById('hashId').getAttribute('value');
-      var ref = new Firebase("https://platon.firebaseio.com/slides/");
+      var ref = new Firebase('https://platon.firebaseio.com/slides/');
       ref.child(hashId).on('value', function(snap){
         slideshow.loadFromString(snap.val().markdown);
       });
